64815d363aa8f668f94fa8b8f0898aefe2b49678
mkoepfli <marco.koepfli@edu.tbz.ch>
2021-11-18 13:44:19 +0100

    added addSubjectActivity with view

---
+++ app/src/main/java/ch/hosttech/schulio/addSubject.java
@@ -0,0 +1,14 @@
+package ch.hosttech.schulio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class addSubject extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_subject);
+    }
+}
