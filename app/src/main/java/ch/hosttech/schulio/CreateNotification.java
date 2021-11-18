698ab269e670a2027d7446ef1b79751adfe6a4b2
mkoepfli <marco.koepfli@edu.tbz.ch>
2021-11-18 09:25:53 +0100

    added CreatNotification and added Intent to CreatNotifiactionActivity in MainActivity

---
+++ app/src/main/java/ch/hosttech/schulio/CreateNotification.java
@@ -0,0 +1,14 @@
+package ch.hosttech.schulio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateNotification extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_notification);
+    }
+}
