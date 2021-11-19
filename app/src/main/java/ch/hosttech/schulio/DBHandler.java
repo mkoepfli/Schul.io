1a2298ce9ed21062d53c494f50a31b4824ea6d71
mkoepfli <marco.koepfli@edu.tbz.ch>
2021-11-19 08:26:46 +0100

    Improved Database

--- app/src/main/java/ch/hosttech/schulio/DBHandler.java
+++ app/src/main/java/ch/hosttech/schulio/DBHandler.java
@@ -27,9 +27,10 @@ public class DBHandler extends SQLiteOpenHelper {
 
     // below variable is for our course name column
     private static final String NAME_COL = "name";
+    private static final String NAME_SUBJECT = "subject";
     private static final String NAME_COL_MARK = "testName";
 
-    private static final String MARK_COL_MARK = "id";
+    private static final String MARK_COL_MARK = "mark";
 
     // creating a constructor for our database handler.
     public DBHandler(Context context) {
@@ -39,33 +40,34 @@ public class DBHandler extends SQLiteOpenHelper {
     // below method is for creating a database by running a sqlite query
     @Override
     public void onCreate(SQLiteDatabase db) {
-        // on below line we are creating 
-        // an sqlite query and we are 
+        // on below line we are creating
+        // an sqlite query and we are
         // setting our column names
         // along with their data types.
-        String query = "CREATE TABLE " + TABLE_NAME + " ("
+
+        db.execSQL("CREATE TABLE " + TABLE_NAME + " ("
                 + ID_COL + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-                + NAME_COL + " TEXT)";
-
-
-        // at last we are calling a exec sql 
-        // method to execute above sql query
-        db.execSQL(query);
+                + NAME_COL + " TEXT)");
+        db.execSQL("CREATE TABLE " + TABLE_NAME_MARK + " ("
+                + ID_COL + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + NAME_SUBJECT + " TEXT, "
+                + NAME_COL_MARK + " TEXT, "
+                + MARK_COL_MARK + " REAL)");
     }
 
     // this method is use to add new course to our sqlite database.
     public void addNewSubject(String subjectName) {
 
-        // on below line we are creating a variable for 
-        // our sqlite database and calling writable method 
+        // on below line we are creating a variable for
+        // our sqlite database and calling writable method
         // as we are writing data in our database.
         SQLiteDatabase db = this.getWritableDatabase();
 
-        // on below line we are creating a 
+        // on below line we are creating a
         // variable for content values.
         ContentValues values = new ContentValues();
 
-        // on below line we are passing all values 
+        // on below line we are passing all values
         // along with its key and value pair.
         values.put(NAME_COL, subjectName);
 
@@ -75,6 +77,21 @@ public class DBHandler extends SQLiteOpenHelper {
 
         // at last we are closing our
         // database after adding database.
+        db.close();
+    }
+
+    public void addNewMark(String subject, String testName, float mark) {
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+
+        values.put(NAME_SUBJECT, subject);
+        values.put(NAME_COL_MARK, testName);
+        values.put(MARK_COL_MARK, mark);
+
+        db.insert(TABLE_NAME_MARK, null, values);
+
         db.close();
     }
 
@@ -101,6 +118,31 @@ public class DBHandler extends SQLiteOpenHelper {
         // and returning our array list.
         cursorSubject.close();
         return subjectModalArrayList;
+    }
+
+    public ArrayList<MarkModal> readMarks(String subjectname) {
+        // on below line we are creating a
+        // database for reading our database.
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        // on below line we are creating a cursor with query to read data from database.
+        Cursor cursorMark = db.rawQuery("SELECT * FROM " + TABLE_NAME_MARK + " WHERE TRIM(subject) = '"+subjectname.trim()+"'", null);
+
+        // on below line we are creating a new array list.
+        ArrayList<MarkModal> markModalArrayList = new ArrayList<>();
+
+        // moving our cursor to first position.
+        if (cursorMark.moveToFirst()) {
+            do {
+                // on below line we are adding the data from cursor to our array list.
+                markModalArrayList.add(new MarkModal(cursorMark.getString(1), cursorMark.getString(2), cursorMark.getFloat(3)));
+            } while (cursorMark.moveToNext());
+            // moving our cursor to next.
+        }
+        // at last closing our cursor
+        // and returning our array list.
+        cursorMark.close();
+        return markModalArrayList;
     }
 
     @Override
---
+++ app/src/main/java/ch/hosttech/schulio/MarkModal.java
@@ -0,0 +1,52 @@
+package ch.hosttech.schulio;
+
+public class MarkModal {
+
+    // variables for our coursename,
+    // description, tracks and duration, id.
+    private String subject;
+    private float mark;
+    private String testName;
+    private int id;
+
+    // creating getter and setter methods
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public float getMark() {
+        return mark;
+    }
+
+    public void setMark(float mark) {
+        this.mark = mark;
+    }
+
+    public String getTestName() {
+        return testName;
+    }
+
+    public void setTestName(String testName) {
+        this.testName = testName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // constructor
+    public MarkModal(String subject, String testName, float mark) {
+        this.subject = subject;
+        this.testName = testName;
+        this.mark = mark;
+    }
+
+}
